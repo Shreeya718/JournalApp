8ff4bd5000485b48b64f933093e44304f79bd34d
Shreeya718 <daspratima545@gmail.com>
2025-08-06 19:45:48 +0530

    Initial commit

---
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,16 @@
+ package net.engineeringdigest.journalApp.controller;
+import org.springframework.web.bind.annotation.RestController;
+ import java.util.Array
+
+
+
+@RestController
+public class JournalEntryController {
+
+    private Map<Long, 
+    public List<JournalEntry> getAll(){
+
+
+    }
+
+}
